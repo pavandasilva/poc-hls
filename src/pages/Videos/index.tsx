42bb66dfadfd00c083fb6520d9b9b6c742c0830d
Pavan <rogerio_pavan@hotmail.com>
2022-01-31 18:41:07 -0300

    multiple resolutions

--- src/pages/Videos/index.tsx
+++ src/pages/Videos/index.tsx
@@ -5,19 +5,22 @@ export const Videos = () => {
   const playerRef = React.useRef<HTMLVideoElement | null>(null);
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>    
+    <div style={{display: 'flex', flexDirection: 'column'}}>  
+    <h2>auto</h2>  
       <div>
         <ReactHlsPlayer
-          src="https://pavantest212.s3.us-east-2.amazonaws.com/file2/index.m3u8"
+          src="https://pavantest212.s3.us-east-2.amazonaws.com/multires/hls/master.m3u8"
           autoPlay
           controls
-          width="270" height="240"
+          width="640" height="360"
           playerRef={playerRef}
           muted
         />
       </div>
+      
+      <h2>Manual</h2>
       <video  width="270" height="240" controls autoPlay muted>
-        <source src="https://pavantest212.s3.us-east-2.amazonaws.com/1.mp4" type="video/mp4"  />
+        <source src="https://pavantest212.s3.us-east-2.amazonaws.com/multires/hls/360_out.m3u8" type="video/mp4"  />
       </video>
     </div>
   )
