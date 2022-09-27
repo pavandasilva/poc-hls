0147d49c80ee4d7d7530f4c2e62ddb005a58d4dc
Pavan <rogerio_pavan@hotmail.com>
2022-09-27 12:06:10 -0300

    feat: add video mp4

--- src/pages/Videos/index.tsx
+++ src/pages/Videos/index.tsx
@@ -1,22 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactHlsPlayer from 'react-hls-player';
 
 export const Videos = () => {
   const playerRef = React.useRef<HTMLVideoElement | null>(null);
 
+  const isVideoPlaying = () => !!(playerRef.current?.currentTime && playerRef.current?.currentTime > 0 && !playerRef.current?.paused && !playerRef.current?.ended && playerRef.current?.readyState > 2);
+
+  const [linkStream, setLinkStream] = useState(()=> "https://d2we8z90utluth.cloudfront.net/files/hls/2/out.m3u8" );
+  const [linkVideo, setLinkVideo] = useState(()=> "https://d2we8z90utluth.cloudfront.net/files/videos/405x720_72f3c1ca-05f3-4e39-a2a8-7021092a7f50.mp4" );
+
   return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>  
-    <h2>auto</h2>  
-      <div>
-        <ReactHlsPlayer
-          src="https://pavantest212.s3.us-east-2.amazonaws.com/multires/hls/master.m3u8"
-          autoPlay
-          controls
-          width="640" height="360"
-          playerRef={playerRef}
-          muted
-        />
-      </div> 
+    <div style={{display: 'flex', flexDirection: 'row', padding: '0px 30px'}}>  
+      <div>{isVideoPlaying()}</div>
+
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <h2>Player HLS</h2>  
+        <label>link .m3u8</label><input type="text" onChange={(event)=> setLinkStream(event.target.value)}/>
+        <caption>Playing {linkStream}</caption>
+        <div>
+          <ReactHlsPlayer
+            src={linkStream}
+            muted
+            autoPlay
+            controls
+            width="349" height="720"
+            playerRef={playerRef}
+
+            hlsConfig={{
+              maxLoadingDelay: 2,
+              minAutoBitrate: 0,
+              lowLatencyMode: true,
+            }}
+          />
+        </div> 
+      </div>
+
+      <div style={{display: 'flex', flexDirection: 'column', padding: '0px 30px'}}>
+      <h2>Player HTML</h2>
+        <label>link .mp4</label><input type="text" onChange={(event)=> setLinkVideo(event.target.value)}/>
+        <caption>Playing {linkVideo}</caption>
+        <div>
+          <video width="349" height="720" controls autoPlay muted>
+            <source 
+              src={linkVideo} 
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      </div>
     </div>
   )
 }
